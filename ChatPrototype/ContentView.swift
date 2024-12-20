c3ce8817fb4ea8e96750fd2513df7783ec4a9b6b
Loc Vu <hiimBugCreator>
2024-12-20 16:48:48 +0700

    Test ne

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, bé Lộc!")
+            Text("Hello, bé Lộc nha!")
         }
         .padding()
     }
